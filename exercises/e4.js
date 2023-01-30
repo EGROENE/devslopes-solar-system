871c52075aa2f3c74a5fcc793d8bbba2f849403b
Ethan <ethangroene@gmail.com>
2023-01-29 21:40:14 -0600

    Reformat code

--- exercises/e4.js
+++ exercises/e4.js
@@ -6,14 +6,12 @@ import { data } from "../data/data";
 
 export function getPlanetsWithLowGravity(data) {
   // Your code goes here...
-  return data.planets.filter(function(planet) {
-    return planet.gravity < 10
-  }).map(function(planet) {
-    return planet.name;
-  })
+  return data.planets
+    .filter(function (planet) {
+      return planet.gravity < 10;
+    })
+    .map((planet) => planet.name);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
