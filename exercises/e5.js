cc34b3755acca7018da809f6eddb045031b9a80f
Ethan <ethangroene@gmail.com>
2023-01-29 21:42:46 -0600

    Reformat code

--- exercises/e5.js
+++ exercises/e5.js
@@ -6,14 +6,12 @@ import { data } from "../data/data";
 
 export function getPlanetsWithMassValue(data, number) {
   // Your code goes here...
-  return data.planets.filter(function(planet) {
-    return planet.mass.massValue >= number;
-  }).map(function(planet) {
-    return planet.name;
-  })
+  return data.planets
+    .filter(function (planet) {
+      return planet.mass.massValue >= number;
+    })
+    .map((planet) => planet.name);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
